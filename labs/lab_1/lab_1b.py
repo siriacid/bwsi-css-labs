1860be19a45a17ca4f45dd067c666ffffb900a14
NZ <Nathanzhoun@gmail.com>
2026-05-03 02:50:54 -0400

    Update lab_1b.py
    DeBug letters in number system

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,6 +37,13 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_sanitized_number(prompt: str) -> float:
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 def main():
     
     print(f"===== Simple Calculator =====") 
